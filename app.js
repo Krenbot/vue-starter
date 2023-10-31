cf559781bee2696ab5de95bf3126e712c585873c
Steven Krenn <stevekrenn@gmail.com>
2023-10-30 20:25:28 -0500

    refactor demo app to counter. add simple event binding

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: "Finish the course and learn Vue!",
-      courseGoalB: "Master Vue and learn amazing apps!",
-      vueLink: "http://vuejs.org",
+      counter: 0,
     };
   },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
-    },
-  },
+  methods: {},
 });
 
-app.mount("#user-goal");
+app.mount("#events");
