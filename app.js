37189a9b3f5168891b5e5d114beb3c61902442b6
Steven Krenn <stevekrenn@gmail.com>
2023-10-31 17:45:07 -0500

    add examples of event modifiers, event jobject, and locking content

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: "",
+      confirmedName: "",
     };
   },
-  methods: {},
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    subtract(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+  },
 });
 
 app.mount("#events");
